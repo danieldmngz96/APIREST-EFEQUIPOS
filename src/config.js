e09db0da7d0a9d06e1f39665e0fb39765ec8e8c5
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2025-04-13 21:59:09 -0500

    correcion config.js

--- src/config.js
+++ src/config.js
@@ -8,11 +8,19 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_DATABASE = process.env.DB_DATABASE || "almacen" ;
 export const DB_PORT = process.env.DB_PORT || 3306;
  */
+// export const PORT = process.env.PORT || 3000;
+// export const DB_HOST = process.env.DB_HOST || "34.136.246.165";
+// export const DB_USER = process.env.DB_USER || "efequipos";
+// export const DB_PASSWORD = process.env.DB_PASSWORD || "}0)L{04h`l^gcV=/";
+// export const DB_DATABASE = process.env.DB_DATABASE || "almacen" ;
+// export const DB_PORT = process.env.DB_PORT || 3306;
+
+
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "34.136.246.165";
-export const DB_USER = process.env.DB_USER || "efequipos";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "}0)L{04h`l^gcV=/";
-export const DB_DATABASE = process.env.DB_DATABASE || "almacen" ;
+export const DB_HOST = process.env.DB_HOST || "sql107.infinityfree.com";
+export const DB_USER = process.env.DB_USER || "if0_38604606";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "K6Lhwj0XjM";
+export const DB_DATABASE = process.env.DB_DATABASE || "if0_38604606_almacen";
 export const DB_PORT = process.env.DB_PORT || 3306;
 
 //bd_efequipos
@@ -23,12 +31,12 @@ export const DB_PASSWORD2 = process.env.DB_PASSWORD2 || "";
 export const DB_DATABASE2 = process.env.DB_DATABASE2 || "bd_efequipos";
 export const DB_PORT2 = process.env.DB_PORT2 || 3306; */
 
-export const PORT2 = process.env.PORT2 || 3000;
-export const DB_HOST2 = process.env.DB_HOST2 || "34.136.246.165";
-export const DB_USER2 = process.env.DB_USER2 || "daniel";
-export const DB_PASSWORD2 = process.env.DB_PASSWORD2 || "Daniel1234*";
-export const DB_DATABASE2 = process.env.DB_DATABASE2 || "bd_efequipos";
-export const DB_PORT2 = process.env.DB_PORT2 || 3306;
+// export const PORT2 = process.env.PORT2 || 3000;
+// export const DB_HOST2 = process.env.DB_HOST2 || "34.136.246.165";
+// export const DB_USER2 = process.env.DB_USER2 || "daniel";
+// export const DB_PASSWORD2 = process.env.DB_PASSWORD2 || "Daniel1234*";
+// export const DB_DATABASE2 = process.env.DB_DATABASE2 || "bd_efequipos";
+// export const DB_PORT2 = process.env.DB_PORT2 || 3306;
 
 /* console.log("REST API en node.JS corriendo en puerto","" + process.env.PORT );
 console.log("conectado a las bases de datos" ," "+  DB_DATABASE ,  process.env.DB_DATABASE2  ); */
